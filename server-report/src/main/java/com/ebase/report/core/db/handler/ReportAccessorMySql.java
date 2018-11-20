0a0302e24247788a5079cc84e9a693a199f28c6e
Lujiawei <ljw_java@aliyun.com>
2018-11-20 17:14:15 +0800

    调试

--- server-report/src/main/java/com/ebase/report/core/db/handler/ReportAccessorMySql.java
+++ server-report/src/main/java/com/ebase/report/core/db/handler/ReportAccessorMySql.java
@@ -1,6 +1,7 @@
 package com.ebase.report.core.db.handler;
 
 import com.ebase.report.common.*;
+import com.ebase.report.core.db.DataBaseType;
 import com.ebase.report.core.db.DataBaseUtil;
 import com.ebase.report.core.db.DataDetailSQL;
 import com.ebase.report.core.db.exception.DbException;
@@ -9,6 +10,7 @@ import com.ebase.report.core.utils.StringUtil;
 import com.ebase.report.core.utils.excel.ExportExcelUtils;
 import com.ebase.report.cube.Dimension;
 import com.ebase.report.model.RptDataField;
+import com.ebase.report.model.RptDataTable;
 import com.ebase.report.model.dynamic.*;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -19,10 +21,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.*;
 
 /**
@@ -463,5 +462,70 @@ public class ReportAccessorMySql extends AbstractAccessor {
 
         return selectSql;
 
+    }
+
+    @Override
+    public List<RptDataTable> queryAllTables(Connection conn, DataBaseType dataBaseType) throws DbException {
+        List<RptDataTable> tables = new ArrayList<RptDataTable>();
+        ResultSet rs = null;
+
+        try {
+
+            DatabaseMetaData dbMetaData = conn.getMetaData();
+
+            // 数据库
+            String catalog = null;
+            // 数据库的用户
+            String schemaPattern = null;// meta.getUserName();
+            // 表名
+            String tableNamePattern = null;
+
+            //可为:"TABLE",   "VIEW",   "SYSTEM   TABLE",
+            //"GLOBAL   TEMPORARY",   "LOCAL   TEMPORARY",   "ALIAS",   "SYNONYM"
+            String[] types = {"TABLE"};/*只要表*/
+
+            if (DataBaseType.DB_TYPE_ORACLE.equals(dataBaseType)) {
+                schemaPattern = dbMetaData.getUserName().toUpperCase();
+            } else if (DataBaseType.DB_TYPE_MYSQL.equals(dataBaseType)) {
+                // MySQL 的 table 这一级别查询不到备注信息
+
+//            }  else if (DATABASETYPE.SQLSERVER.equals(dbtype) || DATABASETYPE.SQLSERVER2005.equals(dbtype)) {
+//                // SqlServer
+//                tableNamePattern = "%";
+//            }  else if (DATABASETYPE.DB2.equals(dbtype)) {
+//                // DB2查询
+//                schemaPattern = "jence_user";
+//                tableNamePattern = "%";
+//            } else if (DATABASETYPE.INFORMIX.equals(dbtype)) {
+//                // SqlServer
+//                tableNamePattern = "%";
+            } else if (DataBaseType.DB_TYPE_HIVE.equals(dataBaseType)) {
+                // hive
+                tableNamePattern = "%";
+            } else {
+                throw new RuntimeException("不认识的数据库类型!");
+            }
+
+            rs = dbMetaData.getTables(null, schemaPattern, tableNamePattern, types);
+
+            String sql = "show table status";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                //只要表名这一列
+                // System.out.println(rs.getObject("TABLE_NAME"));
+                System.out.println(rs.getObject("Name")+"-------->"+rs.getObject("Comment"));
+                RptDataTable rptDataTable = new RptDataTable();
+                rptDataTable.setTableCode(rs.getObject("Name").toString());
+                rptDataTable.setComment(rs.getObject("Comment").toString());
+                tables.add(rptDataTable);
+            }
+        } catch (SQLException e) {
+            throw new DbException(e);
+        } finally {
+            DataBaseUtil.closeResultSet(rs);
+        }
+
+        return tables;
     }
 }
